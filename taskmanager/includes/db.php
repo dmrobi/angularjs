f8ba4dc58a5477c39c2c579c8be9870420e627bf
Robiul Islam Robi <dmrobi89@gmail.com>
2015-10-03 20:14:48 +0600

    few changes

--- taskmanager/includes/db.php
+++ taskmanager/includes/db.php
@@ -2,6 +2,6 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = '';
-$DB_NAME = 'tasks';
+$DB_NAME = 'angular_taskmanager';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
